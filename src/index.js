c6a6b378de91e3430980f1b6062fd74949e0c711
Akhmadali <akhmadalibakhtiyarov@mail.ru>
2023-08-13 16:16:55 +0500

    update: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+        let leftArr=[];
+        let rightArr = [];
+        for(let i=0; i<str.length; i++){
+            if(str[i] === '(' || str[i] === '[' || str[i] === "{"){
+                leftArr.push(str[i]);
+            }
+
+
+            if(str[i] === ')'){
+
+                rightArr.push("(");
+            }else if(str[i] === '}'){
+
+                rightArr.push("{");
+            } else if(str[i] === ']'){
+
+                rightArr.push("[");
+            }
+        }
+
+        rightArr.reverse();
+
+        if(leftArr.length<rightArr.length || leftArr.length>rightArr.length){
+            return false;
+        }
+
+        for(let k=0; k<leftArr.length; k++) {
+            if(leftArr[k] != rightArr[k]){
+                return false;
+            }
+        }
+
+        return true;
+};
